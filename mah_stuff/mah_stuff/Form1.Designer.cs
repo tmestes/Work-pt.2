380f1931445af8829996d752856dd83ede945e94
tmestes <trevorestes706@gmail.com>
2016-03-22 08:19:03 -0400

    added the c# work
    lets hope this can all be put into a folder for convenience

---
+++ mah_stuff/mah_stuff/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace mah_stuff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.storyTextBox = new System.Windows.Forms.TextBox();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.closeFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(620, 404);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(95, 50);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(12, 404);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(86, 50);
+            this.openFileButton.TabIndex = 13;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // storyTextBox
+            // 
+            this.storyTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.storyTextBox.Location = new System.Drawing.Point(12, 6);
+            this.storyTextBox.Multiline = true;
+            this.storyTextBox.Name = "storyTextBox";
+            this.storyTextBox.ReadOnly = true;
+            this.storyTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.storyTextBox.Size = new System.Drawing.Size(703, 392);
+            this.storyTextBox.TabIndex = 12;
+            // 
+            // openFile
+            // 
+            this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFile_FileOk);
+            // 
+            // closeFileButton
+            // 
+            this.closeFileButton.Location = new System.Drawing.Point(104, 404);
+            this.closeFileButton.Name = "closeFileButton";
+            this.closeFileButton.Size = new System.Drawing.Size(86, 50);
+            this.closeFileButton.TabIndex = 14;
+            this.closeFileButton.Text = "Close File";
+            this.closeFileButton.Click += new System.EventHandler(this.closeFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 466);
+            this.Controls.Add(this.closeFileButton);
+            this.Controls.Add(this.storyTextBox);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.exitButton);
+            this.Name = "Form1";
+            this.Text = "Story Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.TextBox storyTextBox;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button closeFileButton;
+    }
+}
+
---
+++ mah_stuff/mah_stuff/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace mah_stuff
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string story;
+                OpenFileDialog openFile;
+                StreamReader inputFile;
+                //page 310
+
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    inputFile = File.OpenText(openFile.FileName);
+
+                    storyTextBox.Text = story;
+                }
+                else
+                {
+                    MessageBox.Show("Operation cancelled.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void openFile_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void closeFileButton_Click(object sender, EventArgs e)
+        {
+            storyTextBox.Text = "";
+        }
+    }
+}
